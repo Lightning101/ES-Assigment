6c0bbf4c517afab642726c737915875ef3264888
chamathravin <chamath30720@gmail.com>
2018-06-27 11:55:04 +0530

    Setup the project

--- Drivers Claim App/Drivers Claim App-war/src/java/Filters/JSPFilter.java
+++ Drivers Claim App/Drivers Claim App-war/src/java/Filters/JSPFilter.java
@@ -8,6 +8,7 @@ package Filters;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -23,6 +24,16 @@ public class JSPFilter implements  Filter{
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req= (HttpServletRequest) request;
         req.getRequestDispatcher("index.jsp").forward(request,response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    
+    }
+    
+    @Override
+    public void destroy() {
+    
     }
     
 }
